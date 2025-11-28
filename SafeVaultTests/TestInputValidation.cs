30c7d72c1efde4e32ac3a982c29ccf112323f5c8
nnorian <kushnirenkoecaterina@gmail.com>
2025-11-28 15:11:34 +0200

    feat: test for sql injection and cros site scription

--- SafeVaultTests/TestInputValidation.cs
+++ SafeVaultTests/TestInputValidation.cs
@@ -1,15 +1,24 @@
-﻿namespace SafeVaultTests;
+﻿using NUnit.Framework;
 
-public class Tests
-{
-    [SetUp]
-    public void Setup()
-    {
+[TestFixture]
+public class TestInputValidation{
+    [Test]
+    public void TestForSQLInjection(){
+        string malicious = "'; DROP TABLE Users; --";
+        string sanitized = InputSanitizer.SanitizeString(malicious);
+
+        Assert.IsFalse(sanitized.Constains("DROP"));
+        Assert.IsFalse(sanitized.Constains("--"));
+        Assert.IsFalse(sanitized.Constains("'"));
     }
 
     [Test]
-    public void Test1()
-    {
-        Assert.Pass();
+    public void TestForXXS(){
+        string malicious = "<script>alert('hacked');</script>";
+        string sanitized = InputSanitizeString(malicious);
+
+        Assert.IsFalse(sanitized.Constains("<script>"));
+        Assert.IsFalse(sanitized.Constains("</script>"));
+        Assert.IsFalse(sanitized.Constains("&lt;") || sanitized.Contains("alert"));
     }
 }
