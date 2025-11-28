651958f7e07e4f6d85d3cd595ae8d80d54442c2e
nnorian <kushnirenkoecaterina@gmail.com>
2025-11-28 14:14:11 +0200

    feat: added the user interaction logic

--- SafeVault/Program.cs
+++ SafeVault/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using SafeVault.Services;
+
+class program{
+    static void Main(){
+        Console.WriteLine("SafeVault terminal app");
+        Console.WriteLine("1. Register");
+        Console.WriteLine("1. Login");
+        Console.WriteLine("1. Register");
+        var choice = Console.ReadLine();
+
+        var repo = new UserRepository(dbconfig.Conn);
+        var auth = new AuthService(repo);
+
+        if (choice == "1"){
+            Console.Write("Usernane:");
+            var user = InputSanitizer.SanitizeString(Console.ReadLine());
+
+            Console.Write("Password: ");
+            var pass = Console.ReadLine();
+
+            auth.Register(user, pass);
+            Console.WriteLine("User registered securely.");
+        }
+
+        else if (choice == "2"){
+            Console.Write("Usernane:");
+            var user = InputSanitizer.SanitizeString(Console.ReadLine());
+
+            Console.Write("Password: ");
+            var pass = Console.ReadLine();
+
+            if (auth.Login(user, pass))
+                Console.WriteLine("Login successful.");
+            else
+                Console.WriteLine("Invalid login attempt.");
+        }
+    }
+}
